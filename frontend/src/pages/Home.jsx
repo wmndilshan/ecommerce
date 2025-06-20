aa7b54712fc4405faa109b13b58b8146d90d2f87
Nuwan Dilshan <wmndilshan@gmail.com>
2025-06-21 01:53:41 +0530

    hero component completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Hero />
+    </div>
+  );
+};
 
-export default Home
+export default Home;

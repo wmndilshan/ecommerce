1c390e5d1adea76e6650406ce7bed41e1c449405
Nuwan Dilshan <wmndilshan@gmail.com>
2025-06-21 00:43:34 +0530

    navbar compnent completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
